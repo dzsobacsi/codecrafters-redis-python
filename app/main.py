6e0c059c94df5383b7869c8c6bd1a1072fa992da
dzsobacsi <tberki77@gmail.com>
2024-06-26 16:35:10 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,7 +11,8 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
 
-    connection.sendall(b"+PONG\r\n")
+    while connection.recv(1024):
+        connection.sendall(b"+PONG\r\n")    
 
 
 if __name__ == "__main__":
