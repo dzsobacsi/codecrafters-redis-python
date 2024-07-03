d6fc1c47d96f8ba2811c9f9da31bd2c5e7b3c057
dzsobacsi <tberki77@gmail.com>
2024-07-03 20:13:25 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,22 +1,38 @@
 import asyncio
 
+
 def parse(command: str):
-    return [w for w in command.split('\r\n') if w != '' and w[0] not in ['*', '$', ':', '+', '-']]
+    return [
+        w 
+        for w in command.split('\r\n') 
+        if w != '' and w[0] not in '*$:+-'
+    ]
 
 
 async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    memory = {}
+
     while data := await reader.read(1024):
         input = parse(data.decode())
-        command = input[0]
+        command = input[0].upper()
         args = input[1:]
 
         if command == 'PING':
             writer.write(b"+PONG\r\n")
-            await writer.drain()
 
         elif command == 'ECHO':
             writer.write(f"+{args[0]}\r\n".encode('utf-8'))
-            await writer.drain()
+
+        elif command == 'SET':
+            memory[args[0]] = args[1]
+            writer.write(b"+OK\r\n")
+
+        elif command == 'GET':
+            value = memory.get(args[0], None)
+            response = f"+{value}\r\n" if value else "$-1\r\n"
+            writer.write(response.encode('utf-8'))
+        
+        await writer.drain()
 
 
 async def start_server(port: int, host: str = 'localhost'):
