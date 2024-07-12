50f98b70a112027fc538dd564cd666ff093a5d6d
dzsobacsi <tberki77@gmail.com>
2024-07-12 15:20:10 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import asyncio
 
+memory = {}
 
 def parse(command: str):
     return [
@@ -9,34 +10,44 @@ def parse(command: str):
     ]
 
 
-async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-    memory = {}
+async def delayed_mem_purge(key: str, ms: int):
+    await asyncio.sleep(ms / 1000)
+    memory.pop(key, None)
 
+
+async def get_response(command: str, *args):
+    if command == 'PING':
+        return "+PONG\r\n"
+
+    elif command == 'ECHO':
+        return f"+{args[0]}\r\n"
+
+    elif command == 'SET':
+        memory[args[0]] = args[1]
+        if len(args) >= 4 and args[2].upper() == 'PX':
+            coroutine = delayed_mem_purge(args[0], int(args[3]))
+            asyncio.create_task(coroutine)
+        return "+OK\r\n"
+
+    elif command == 'GET':
+        value = memory.get(args[0])
+        return f"+{value}\r\n" if value else "$-1\r\n"
+
+    return "-ERR unknown command\r\n"
+
+
+async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     while data := await reader.read(1024):
         input = parse(data.decode())
         command = input[0].upper()
         args = input[1:]
-
-        if command == 'PING':
-            writer.write(b"+PONG\r\n")
-
-        elif command == 'ECHO':
-            writer.write(f"+{args[0]}\r\n".encode('utf-8'))
-
-        elif command == 'SET':
-            memory[args[0]] = args[1]
-            writer.write(b"+OK\r\n")
-
-        elif command == 'GET':
-            value = memory.get(args[0], None)
-            response = f"+{value}\r\n" if value else "$-1\r\n"
-            writer.write(response.encode('utf-8'))
-        
+        response = await get_response(command, *args)
+        writer.write(response.encode('utf-8'))
         await writer.drain()
 
 
 async def start_server(port: int, host: str = 'localhost'):
-    server = await asyncio.start_server(handle_client, host=host, port=port)
+    server = await asyncio.start_server(handle_client, host, port)
     async with server:
         print(f"Listening on {host}, port: {port} ")
         await server.serve_forever()
